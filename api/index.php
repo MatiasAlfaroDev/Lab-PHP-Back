3e5e34b3c760b6206b769f342c54aa6ff73a5e3b
MatiasAlfaroDev <malfarodev@gmail.com>
2026-06-10 20:09:25 -0300

    HOTFIX - CORS & API Fixes

--- api/index.php
+++ api/index.php
@@ -1,3 +1,8 @@
 <?php
 
+// Evita que Symfony interprete "/api" como base path y recorte
+// el prefijo de las rutas /api/* (el front controller vive en /api/index.php).
+$_SERVER['SCRIPT_NAME'] = '/index.php';
+$_SERVER['SCRIPT_FILENAME'] = __DIR__.'/../public/index.php';
+
 require __DIR__.'/../public/index.php';
